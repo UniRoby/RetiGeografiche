20c736181514aff8bc095e187b6bfe63aee5c6ae
Roberto Piscopo <r.piscopo8@studenti.unisa.it>
2024-02-17 11:27:58 +0100

    extracted other values by topics

--- AnalisiDataset.py
+++ AnalisiDataset.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Leggi il file CSV
-df = pd.read_csv('commenti_dataset_a.csv')
+df = pd.read_csv('commenti_dataset.csv')
 
 # Numero di righe negative per ogni notizia/titolo e giornale per social
 num_commenti_negativi_per_titolo_e_giornale = df[df['sentiment'] == 'negativo'].groupby(['giornale', 'titolo','social']).size().reset_index(name='num_commenti_negativi')
@@ -20,6 +20,8 @@ num_commenti_positivi_per_titolo_e_giornale = commenti_positivi.groupby(['giorna
 # Calcola la media per ciascun giornale suddivisio per social
 media_positivi_per_giornale = num_commenti_positivi_per_titolo_e_giornale.groupby(['giornale','social'])['num_commenti_positivi'].mean()
 
+# Filtra solo i commenti con hate_speech nelle categorie 'inappropriato', 'offensivo' e 'violento'
+commenti_hate_speech = df[df['hate_speech'].isin(['inappropriato', 'offensivo', 'violento'])]
 
 for giornale, media_negativi in media_negativi_per_giornale.items():
     print(f'Social, Giornale: {giornale}, Media Commenti Negativi: {media_negativi:.2f}')
@@ -42,6 +44,12 @@ commenti_positivi_negativi_per_notizia = df.groupby(['titolo','giornale', 'socia
 print("\n(Creare Excel) Numero di commenti positivi e negativi per ogni notizia:")
 print(commenti_positivi_negativi_per_notizia)
 
+# Numero di commenti di odio per ogni notizia suddivise per giornale e topic (titolo) per social
+commenti_odio_per_notizia = commenti_hate_speech.groupby(['titolo', 'giornale','social']).size().reset_index(name='hate_speech')
+# Stampa dei risultati
+print("\nNumero di commenti odio per ogni notizia (suddivisi per giornale e social:")
+print(commenti_odio_per_notizia)
+
 # Commenti negativi e positivi per ogni topic
 commenti_per_topic = df.groupby(['topic','social','sentiment']).size().unstack(fill_value=0).reset_index()
 print("\nCommenti negativi e positivi per ogni topic per social:")
@@ -52,8 +60,7 @@ commenti_hS_per_topic = df.groupby(['topic','social','hate_speech']).size().unst
 print("\nCommenti hate speech per ogni topic per social suddivisi per categoria di odio:")
 print(commenti_hS_per_topic)
 
-# Filtra solo i commenti con hate_speech nelle categorie 'inappropriato', 'offensivo' e 'violento'
-commenti_hate_speech = df[df['hate_speech'].isin(['inappropriato', 'offensivo', 'violento'])]
+
 # Conta il numero di commenti odio per ciascun topic
 #num_commenti_hate_per_topic = commenti_hate_speech['topic'].value_counts()
 num_commenti_hate_per_topic =commenti_hate_speech.groupby(['topic','social']).size().reset_index(name='num_hate_topic')
@@ -71,3 +78,74 @@ print("\nTopic con il massimo numero di commenti di odio per ogni social:")
 print(topic_max_hate_per_social)
 
 
+#-------------------------CRONACA-----------------------
+
+cronaca = df[df['topic'].str.lower() == 'cronaca']
+# Numero di commenti per ogni notizia (titolo) per social TOPIC=CRONACA
+commenti_positivi_negativi_per_notizia_cronaca= cronaca.groupby(['topic','titolo','giornale', 'social','sentiment']).size().unstack(fill_value=0).reset_index()
+print("\n(Creare Excel) Numero di commenti positivi e negativi per ogni notizia (CRONACA):")
+print(commenti_positivi_negativi_per_notizia_cronaca)
+
+# Numero di commenti per ogni notizia suddivise per giornale e topic (titolo) per social TOPIC=CRONACA
+commenti_per_notizia_cronaca = cronaca.groupby(['titolo', 'giornale','social']).size().reset_index(name='num_commenti')
+# Stampa dei risultati
+print("\nNumero di commenti per ogni notizia (suddivisi per giornale e social) (cronaca):")
+print(commenti_per_notizia_cronaca)
+
+# Filtra solo i commenti con hate_speech nelle categorie 'inappropriato', 'offensivo' e 'violento'
+commenti_hate_speech_cronaca = cronaca[cronaca['hate_speech'].isin(['inappropriato', 'offensivo', 'violento'])]
+
+# Numero di commenti di odio per ogni notizia suddivise per giornale e topic (titolo) per social
+commenti_odio_per_notizia_cronaca = commenti_hate_speech_cronaca.groupby(['titolo', 'giornale','social']).size().reset_index(name='hate_speech')
+# Stampa dei risultati
+print("\nNumero di commenti odio per ogni notizia (suddivisi per giornale e social  (cronaca) :")
+print(commenti_odio_per_notizia_cronaca)
+
+#-------------------------CRONACA NERA-----------------------
+
+cronaca_nera = df[df['topic'].str.lower() == 'cronaca nera']
+# Numero di commenti per ogni notizia (titolo) per social TOPIC=CRONACA_NERA
+commenti_positivi_negativi_per_notizia_cronaca_nera= cronaca_nera.groupby(['topic','titolo','giornale', 'social','sentiment']).size().unstack(fill_value=0).reset_index()
+print("\n(Creare Excel) Numero di commenti positivi e negativi per ogni notizia (CRONACA NERA):")
+print(commenti_positivi_negativi_per_notizia_cronaca_nera)
+
+# Numero di commenti per ogni notizia suddivise per giornale e topic (titolo) per social TOPIC=cronaca_nera
+commenti_per_notizia_cronaca_nera = cronaca_nera.groupby(['titolo', 'giornale','social']).size().reset_index(name='num_commenti')
+# Stampa dei risultati
+print("\nNumero di commenti per ogni notizia (suddivisi per giornale e social) (cronaca_nera):")
+print(commenti_per_notizia_cronaca_nera)
+
+# Filtra solo i commenti con hate_speech nelle categorie 'inappropriato', 'offensivo' e 'violento'
+commenti_hate_speech_cronaca_nera = cronaca_nera[cronaca_nera['hate_speech'].isin(['inappropriato', 'offensivo', 'violento'])]
+
+# Numero di commenti di odio per ogni notizia suddivise per giornale e topic (titolo) per social
+commenti_odio_per_notizia_cronaca_nera = commenti_hate_speech_cronaca_nera.groupby(['titolo', 'giornale','social']).size().reset_index(name='hate_speech')
+# Stampa dei risultati
+print("\nNumero di commenti odio per ogni notizia (suddivisi per giornale e social  (cronaca_nera) :")
+print(commenti_odio_per_notizia_cronaca_nera)
+
+
+#-------------------------POLITICA-----------------------
+
+politica = df[df['topic'].str.lower() == 'politica']
+# Numero di commenti per ogni notizia (titolo) per social TOPIC=politica
+commenti_positivi_negativi_per_notizia_politica= politica.groupby(['topic','titolo','giornale', 'social','sentiment']).size().unstack(fill_value=0).reset_index()
+print("\n(Creare Excel) Numero di commenti positivi e negativi per ogni notizia (POLITICA):")
+print(commenti_positivi_negativi_per_notizia_politica)
+
+
+
+# Numero di commenti per ogni notizia suddivise per giornale e topic (titolo) per social TOPIC=politica
+commenti_per_notizia_politica = politica.groupby(['titolo', 'giornale','social']).size().reset_index(name='num_commenti')
+# Stampa dei risultati
+print("\nNumero di commenti per ogni notizia (suddivisi per giornale e social) (politica):")
+print(commenti_per_notizia_politica)
+
+# Filtra solo i commenti con hate_speech nelle categorie 'inappropriato', 'offensivo' e 'violento'
+commenti_hate_speech_politica = politica[politica['hate_speech'].isin(['inappropriato', 'offensivo', 'violento'])]
+
+# Numero di commenti di odio per ogni notizia suddivise per giornale e topic (titolo) per social
+commenti_odio_per_notizia_politica= commenti_hate_speech_politica.groupby(['titolo', 'giornale','social']).size().reset_index(name='hate_speech')
+# Stampa dei risultati
+print("\nNumero di commenti odio per ogni notizia (suddivisi per giornale e social  (politica) :")
+print(commenti_odio_per_notizia_politica)
